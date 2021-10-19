226bc5735e0a103387c6ce0bfe828ab330db67fe
Madkodevian <madkodevian@gmail.com>
2021-10-19 12:51:07 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './views/list/list.component';
+import { AboutUsComponent } from './views/about-us/about-us.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { LoginComponent } from './views/login/login.component';
+const routes: Routes = [
+{ path: 'list', component: ListComponent },
+{ path: 'us', component: AboutUsComponent },
+{ path: 'login', component: LoginComponent},
 
-const routes: Routes = [];
-
+{ path: '', redirectTo: '/list', pathMatch:'full'},
+{ path:'**', component:PageNotFoundComponent}
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule],
+imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule { }
+
